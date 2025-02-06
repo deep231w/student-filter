8fdb11f700ed98c91beef67546f7daca08c20251
Deepak kumar maharana <100968930+deep231w@users.noreply.github.com>
2025-02-07 02:28:49 +0530

    route fixed

--- backend/src/routes/userDetails.ts
+++ backend/src/routes/userDetails.ts
@@ -2,7 +2,7 @@ import express ,{Response, Request} from "express";
 import prisma from "../db/db";
 const router=express.Router();
 
-router.get("/user", async(req:Request, res:Response)=>{
+router.get("/student", async(req:Request, res:Response)=>{
     console.log("inside route");
     try{
         const users= await prisma.user.findMany({
@@ -23,8 +23,22 @@ router.get("/user", async(req:Request, res:Response)=>{
 
     }
 })
-router.get("/filter",async(req:Request, res:Response)=>{
-
+router.get("/student/:id",async(req:Request, res:Response)=>{
+    const {id}=req.params;
+    try{
+        const response= await prisma.user.findFirst({
+            where:{
+                id:Number(id)
+            }
+        })
+        res.status(200).json(response);
+        
+    }catch(e){
+        console.log("error in student card route ",e);
+        res.status(500).json({
+            error:"server error , rout= /student/:id"
+        })
+    }
 })
 
 
