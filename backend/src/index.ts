86df5673e0a985441bc009ba64535ecaacdf6489
Deepak kumar maharana <100968930+deep231w@users.noreply.github.com>
2025-02-07 01:12:16 +0530

    cors error fixed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,16 @@
 import express,{Response,Request } from "express";
 import userDetails from "./routes/userDetails"
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 const app=express();
+
+app.use(cors({
+    origin:"http://localhost:5173",
+    methods:['GET','POST','DELET','PUT'],
+    credentials:true
+}))
 
 app.use(express.json());
 
